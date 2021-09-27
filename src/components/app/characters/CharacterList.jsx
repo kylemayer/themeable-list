7beb53ef1a8aed612cbf8e0d2d29dac1c0d842c9
Kyle Mayer <x@kylemayer.dev>
2021-09-27 15:24:33 -0700

    provider and context setup

--- src/components/app/characters/CharacterList.jsx
+++ src/components/app/characters/CharacterList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAvatars } from '../../../services/useAvatars';
 import Characters from './Characters';
+import '../../../styles/main.css';
 
 
 const CharacterList = () => {
---
+++ src/components/context/ModeProvider.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useContext, createContext } from 'react';
+import PropTypes from 'prop-types';
+
+const ModeContext = createContext();
+
+const ModeProvider = ({ children }) => {
+    const [mode, setMode] = useState(true);
+
+    const toggleDarkMode = () = setMode((prevState) => !prevState.darkMode);
+
+    const setting = { mode, toggleDarkMode };
+
+    return (
+        <ModeContext.Provider value={setting}>
+            {children}
+        </ModeContext.Provider>
+    );
+};
+
+export const useMode = () => {
+    const { mode } = useContext(ModeContext);
+    return mode;
+};
+
+export const useToggleMode = () => {
+    const { toggleDarkMode } = useContext(ModeContext);
+    return toggleDarkMode;
+};
+
+ModeProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default ModeProvider;
+
+
